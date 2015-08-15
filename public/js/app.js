d56b6bf044d04fa82636714115318fd1bf3a4414
Alex Rib <alexis@3rdsense.com>
2015-08-15 15:41:12 +1000

    clean

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,6 @@
 var app = angular.module('todoApp', [], function($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
-
-    /*$httpProvider.defaults.transformRequest = function(data){
-        if (data === undefined) {
-            return data;
-        }
-        return $.param(data);
-    }8*/
-
 });
 
 app.controller('todoController', function($scope, $http) {
@@ -25,8 +17,6 @@ app.controller('todoController', function($scope, $http) {
                 $scope.todos = data;
                 $scope.loading = false;
                 console.log(data);
-
-
             });
     }
 
@@ -49,30 +39,7 @@ app.controller('todoController', function($scope, $http) {
             $scope.errors.push(status);
             console.log('error');
         });
-
-
     };
-
-    $scope.SignUp = function() {
-
-        $scope.errors.splice(0, $scope.errors.length); // remove all error messages
-        $scope.msgs.splice(0, $scope.msgs.length);
-
-        $http.post('post_es.php', {'uname': $scope.username, 'pswd': $scope.userpassword, 'email': $scope.useremail}
-        ).success(function(data, status, headers, config) {
-                if (data.msg != '')
-                {
-                    $scope.msgs.push(data.msg);
-                }
-                else
-                {
-                    $scope.errors.push(data.error);
-                }
-            }).error(function(data, status) { // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                $scope.errors.push(status);
-            });
-    }
 
     $scope.updateTodo = function(todo) {
         $scope.loading = true;
@@ -96,7 +63,6 @@ app.controller('todoController', function($scope, $http) {
             .success(function() {
                 $scope.todos.splice(index, 1);
                 $scope.loading = false;
-
             });;
     };
 
